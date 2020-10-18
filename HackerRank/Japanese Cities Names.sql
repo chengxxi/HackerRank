d38910ec9e6ff26caf287664c30c77e68bd20a8d
chengxxi <71359467+chengxxi@users.noreply.github.com>
2020-10-18 22:32:10 +0900

    HackerRank : Japanese Cities' Names

---
+++ HackerRank/Japanese Cities Names.sql
@@ -0,0 +1,11 @@
+/* Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN. */
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+/* EXPECTED OUTPUT
+Neyagawa 
+Ageo 
+Sayama 
+Omuta 
+Tokuyama 
+*/
