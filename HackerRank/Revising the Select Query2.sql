e2b27f0b0b6b367e159ab7da2321443d92341e03
chengxxi <heeya121z@naver.com>
2020-10-18 18:11:38 +0900

    HackerRank : Revising the Select Query2

--- HackerRank/Revising the Select Query2.sql
+++ HackerRank/Revising the Select Query2.sql
@@ -4,9 +4,8 @@ SELECT NAME FROM CITY
 WHERE POPULATION > 120000 AND COUNTRYCODE = "USA";
 
 /* EXPECTED OUTPUT
-3878 Scottsdale USA Arizona 202705 
-3965 Corona USA California 124966 
-3973 Concord USA California 121780 
-3977 Cedar Rapids USA Iowa 120758 
-3982 Coral Springs USA Florida 117549
+Scottsdale
+Corona
+Concord
+Cedar Rapids
 */
