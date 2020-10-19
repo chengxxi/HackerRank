c32935c0322d4a600d20f361ec1f76beadb313ca
chengxxi <71359467+chengxxi@users.noreply.github.com>
2020-10-20 00:16:03 +0900

    HackerRank : Weather Observation Station6

---
+++ HackerRank/Weather Observation Station 6.sql
@@ -0,0 +1,71 @@
+/* Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates. */
+
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) IN ('a','e','i','o','u');
+
+/* EXPECTED OUTPUT
+Acme 
+Addison 
+Agency 
+Aguanga 
+Alanson 
+Alba 
+Albany 
+Albion 
+Algonac 
+Aliso Viejo 
+Allerton 
+Alpine 
+Alton 
+Amazonia 
+Amo 
+Andersonville 
+Andover 
+Anthony 
+Archie 
+Arispe 
+Arkadelphia 
+Arlington 
+Arrowsmith 
+Athens 
+Atlantic Mine 
+Auburn 
+East China 
+East Haddam 
+East Irvine 
+Eastlake 
+Edgewater 
+Effingham 
+Eleele 
+Elkton 
+Elm Grove 
+Emmett 
+Equality 
+Eriline 
+Ermine 
+Eros 
+Eskridge 
+Esmond 
+Eufaula 
+Eureka Springs 
+Eustis 
+Everton 
+Irvington 
+Oakfield 
+Oconee 
+Odin 
+Ojai 
+Olmitz 
+Onaway 
+Orange City 
+Orange Park 
+Osage City 
+Osborne 
+Oshtemo 
+Ottertail 
+Ozona 
+Udall 
+Ukiah 
+Union Star 
+Upperco 
+Urbana
+*/
